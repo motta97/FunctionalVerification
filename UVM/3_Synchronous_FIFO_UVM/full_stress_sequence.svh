1612960a96bd562494a482f7526117b23cba8135
Mostafa <mabdelsattar97@gmail.com>
2026-09-19 17:59:14 +0300

    fix bugs

--- UVM/3_Synchronous_FIFO_UVM/full_stress_sequence.svh
+++ UVM/3_Synchronous_FIFO_UVM/full_stress_sequence.svh
@@ -6,25 +6,20 @@ function new(string name="full_stress_sequence");
     super.new(name);
 endfunction
 
-function void build_phase(uvm_phase phase);
-
-    command_transaction = command_sequence_item#(WIDTH)::type_id::create("command_transaction");
-
-
-endfunction
 
 task body();
-
+ command_transaction = command_sequence_item#(WIDTH)::type_id::create("command_transaction");
 start_item(command_transaction);
-command_transaction.rst_n=0;
+    command_transaction.rst_n=0;
 finish_item(command_transaction);
 
 repeat(DEPTH*2)begin
+    command_transaction = command_sequence_item #(WIDTH)::type_id::create("command_transaction");
     start_item(command_transaction);
-        command_transaction.rst_n<=1;
-        command_transaction.push <= 1;
-        command_transaction.pop <=0;
-        command_transaction.push_data <= 1;//can be any number in the valid range
+        command_transaction.rst_n=1;
+        command_transaction.push = 1;
+        command_transaction.pop =0;
+        command_transaction.push_data = 1;//can be any number in the valid range
     finish_item(command_transaction);
 end
 
--- UVM/3_Synchronous_FIFO_UVM/full_stress_test.svh
+++ UVM/3_Synchronous_FIFO_UVM/full_stress_test.svh
@@ -7,24 +7,36 @@ fifo_sequencer fifo_sequencer_h;
 full_stress_sequence full_stress_sequence_h;
 
 function new(string name, uvm_component parent);
-super.new(name, parent);
+    super.new(name, parent);
+
 endfunction
 
 function void build_phase(uvm_phase phase);
-    env_h = env::type_id::create("env_h", this);
     agent_config_h = new(UVM_ACTIVE);
     uvm_config_db #(agent_config)::set(null, "*","agent_config_h", agent_config_h);
-
+    env_h = env::type_id::create("env_h", this);
+    full_stress_sequence_h = new("full_stress_sequence_h");
 endfunction
 
 function void end_of_elaboration_phase(uvm_phase phase);
+    `uvm_info(
+        "full_stress_test", "I got here at end of elabor. phase", UVM_LOW
+    )
     fifo_sequencer_h = env_h.agent_h.fifo_sequencer_h;
+    if(fifo_sequencer_h == null)begin
+        `uvm_info("full_stress_test", "sequencer is null", UVM_LOW)
+    end
+    if(full_stress_sequence_h == null)begin
+        `uvm_info("full_stress_test", "sequence itself is null", UVM_LOW)
+    end
 endfunction
 
 task run_phase(uvm_phase phase);
-
+    `uvm_info(
+        "full_stress_test", "I got here at run phase", UVM_LOW
+    )
     phase.raise_objection(this);
-        full_stress_sequence_h = new("full_stress_sequence_h");
+
         full_stress_sequence_h.start(fifo_sequencer_h);
 
     phase.drop_objection(this);
